e7ff0e79349d73fbd183b548b69ed7a01de1c3ca
Jérôme Mahuet <jerome.mahuet@gmail.com>
2016-10-16 16:40:14 +0200

    clean

--- src/components/searchbar-compiled.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by rydgel on 16/10/2016.
- */
-"use strict";
-
-//# sourceMappingURL=searchbar-compiled.js.map
--- src/components/video_detail-compiled.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by rydgel on 16/10/2016.
- */
-"use strict";
-
-//# sourceMappingURL=video_detail-compiled.js.map
